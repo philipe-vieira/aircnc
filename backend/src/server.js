c0f255fcf1ef03632092409ee2d6eeea3bd550fb
Philipe Vieira <philipefilho@hotmail.com>
2019-10-04 17:38:07 -0300

    Alterações para o frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require( 'express' );
 const mongoose = require( 'mongoose' );
+const cors = require('cors');
+const path = require('path');
+
 const routes = require( './routes' );
 const app = express();
 
@@ -9,6 +12,8 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@cluster0-ukwwy.mongodb.net/omnista
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 app.listen(3333);
